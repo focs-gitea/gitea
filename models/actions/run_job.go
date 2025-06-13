cbfd40df16d9f7a82416db6612f544a2d7351d0d
BoYanZh <boyanzh233@gmail.com>
2025-06-13 09:43:43 -0400

    feat: add triggering_actor field to actions

--- models/actions/run_job.go
+++ models/actions/run_job.go
@@ -144,7 +144,10 @@ func UpdateRunJob(ctx context.Context, job *ActionRunJob, cond builder.Cond, col
 		if run.Stopped.IsZero() && run.Status.IsDone() {
 			run.Stopped = timeutil.TimeStampNow()
 		}
-		if err := UpdateRun(ctx, run, "status", "started", "stopped"); err != nil {
+		if job.Run.TriggerUserID != 0 {
+			run.TriggerUserID = job.Run.TriggerUserID
+		}
+		if err := UpdateRun(ctx, run, "status", "started", "stopped", "trigger_user_id"); err != nil {
 			return 0, fmt.Errorf("update run %d: %w", run.ID, err)
 		}
 	}
